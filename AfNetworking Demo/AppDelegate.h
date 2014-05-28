22ffa8e0f911e4342f4ffcf8d0577156a06386fb
Dottechnologies <pchandra@DOTmac3.local>
2014-05-28 11:10:23 +0530

    Initial Commit

---
+++ AfNetworking Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AfNetworking Demo
+//
+//  Created by Dottechnologies on 5/28/14.
+//  Copyright (c) 2014 Dottechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AfNetworking Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AfNetworking Demo
+//
+//  Created by Dottechnologies on 5/28/14.
+//  Copyright (c) 2014 Dottechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
